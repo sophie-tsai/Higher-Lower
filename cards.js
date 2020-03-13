de038582b5c23efc6745b6de9ce5d6471a87b13e
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-03-12 17:57:40 -0700

    Intial Commit

---
+++ cards.js
@@ -0,0 +1,22 @@
+function success(json){
+	const img = document.querySelector("#deckOfCards");
+	console.log(json);
+	img.src = json.cards[0].image;
+}
+
+function error(err){
+	const errorMessage = document.querySelector("#errorMsg");
+	errorMessage.innerText = "Error" + err;
+}
+
+function drawNew(){
+
+	const promise = fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=1");
+	promise.then((response)=> response.json())
+		.then(success);
+	promise.catch(error);
+}
+
+drawNew();
+
+document.querySelector("#instructions").addEventListener("click", drawNew);
